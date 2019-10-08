90fa1be42e2967cb0761f0cb141d9516936fa0b4
Dhruv Jain <dhruvjainpenny@gmail.com>
2019-10-08 18:09:52 +0530

    signup panel responsive

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Alert, Button, FlexboxGrid, Panel, Form, FormGroup, ControlLabel, FormControl } from 'rsuite';
+import { Alert, Button, Panel, Form, FormGroup, ControlLabel, FormControl, Grid, Row, Col } from 'rsuite';
 import SignupContainer from '../stores/signup';
 
 function Signup() {
@@ -12,29 +12,32 @@ function Signup() {
     Alert.error(Signup.error, 3000)
   }
   return (
-    <FlexboxGrid justify="center">
-      <FlexboxGrid.Item colspan={12}>
-        <Panel header={<h3>Create an Account</h3>}>
-          <Form fluid>
-            <FormGroup>
-              <ControlLabel>Name</ControlLabel>
-              <FormControl onChange={(value) => Signup.handleChange('name', value)} name="name" />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Email</ControlLabel>
-              <FormControl onChange={(value) => Signup.handleChange('email', value)} name="email" type="email" />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Password</ControlLabel>
-              <FormControl onChange={(value) => Signup.handleChange('password', value)} name="password" type="password" />
-            </FormGroup>
-            <FormGroup>
-              <Button loading={Signup.loading} onClick={Signup.submit} appearance="primary">Submit</Button>
-            </FormGroup>
-          </Form>
-        </Panel>
-      </FlexboxGrid.Item>
-    </FlexboxGrid>
+    <Grid>
+      <Row>
+        <Col md={6}></Col>
+        <Col md={12}>
+          <Panel header={<h3>Create an Account</h3>}>
+            <Form fluid>
+              <FormGroup>
+                <ControlLabel>Name</ControlLabel>
+                <FormControl onChange={(value) => Signup.handleChange('name', value)} name="name" />
+              </FormGroup>
+              <FormGroup>
+                <ControlLabel>Email</ControlLabel>
+                <FormControl onChange={(value) => Signup.handleChange('email', value)} name="email" type="email" />
+              </FormGroup>
+              <FormGroup>
+                <ControlLabel>Password</ControlLabel>
+                <FormControl onChange={(value) => Signup.handleChange('password', value)} name="password" type="password" />
+              </FormGroup>
+              <FormGroup>
+                <Button loading={Signup.loading} onClick={Signup.submit} appearance="primary">Submit</Button>
+              </FormGroup>
+            </Form>
+          </Panel>
+        </Col>
+      </Row>
+    </Grid>
   )
 }
 
